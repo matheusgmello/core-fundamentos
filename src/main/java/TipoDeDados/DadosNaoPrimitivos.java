b6864dd3b25c597bb1e7f58a2cc7c0bc596dfde4
Matheus Mello <mathgfmello@gmail.com>
2025-02-25 19:18:04 -0300

    feat: Adicionando Dados Não Primitivos

---
+++ src/main/java/TipoDeDados/DadosNaoPrimitivos.java
@@ -0,0 +1,16 @@
+package TipoDeDados;
+
+public class DadosNaoPrimitivos {
+    public static void main(String[] args) {
+    // Tudo que for digitado aqui dentro com o comando PSVM vai ser compilado pelo Java
+
+        /*
+        * Dados nao primitivos: String, Array, Class, Enum
+        * Objetivo: Criar um ninja, e atribuir metodos a ele.
+        *
+        * */
+
+        String nome = "Naruto Uzacrack";
+        System.out.println(nome.toUpperCase());;
+    }
+}
