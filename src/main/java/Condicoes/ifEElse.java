6555e6fe8adb7453a9cf95e8f2df27d33ebbe0c4
Matheus Mello <mathgfmello@gmail.com>
2025-02-26 12:53:15 -0300

    feat: Adicionando Condicionais

---
+++ src/main/java/Condicoes/ifEElse.java
@@ -0,0 +1,31 @@
+package Condicoes;
+
+public class ifEElse {
+    public static void main(String[] args) {
+
+        /*
+         * IF e ELSE - Condicoes
+         * else if -
+         * Objetivo: Passar o ninja de nivel de acordo com o numero de missoes
+         * */
+
+        // Ninja naruto
+        String nome = "Naruto Uzumaki";
+        String rank;
+
+        int idade = 10;
+        boolean hokage = false;
+        short numeroDeMissoes = 20;
+
+
+        //se(condicao) {faca isso}
+        if(numeroDeMissoes == 10 && idade > 15){
+            System.out.println("Rank: Chunnin");
+        } else if (numeroDeMissoes >= 20) {
+            System.out.println("Rank: Jounnin");
+        }
+        else {
+            System.out.println("Rank: Gennim");
+        }
+    }
+}
