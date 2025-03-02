228f2e9b8bcc541ea5c2a75981897840989db9a3
Matheus Mello <mathgfmello@gmail.com>
2025-03-02 17:32:21 -0300

    feat: Adicionado While e For

---
+++ src/main/java/Condicoes/LacosDeRepeticao.java
@@ -0,0 +1,30 @@
+package Condicoes;
+
+public class LacosDeRepeticao {
+    public static void main(String[] args) {
+        /*
+        * Laços de repetição: vao repetir infinitamente ou até que uma condição seja atendida
+        * WHILE - For
+        * */
+
+        // WHILE
+        // WHILE - (condicao) {Tudo auqi vai acontecer}
+
+        int numeroDeClones = 0;
+        int numeroMaximoDeClones = 10;
+
+//        while (numeroDeClones <= numeroMaximoDeClones){
+//            numeroDeClones++;
+//            System.out.println("O naruto fez um clone das - " + numeroDeClones);
+//        }
+
+        // FOR
+
+        for (int i = 0; i <= numeroMaximoDeClones; i++) {
+            System.out.println("O naruto está louco e esta se clonando - " + i);
+        }
+
+    }
+
+
+}
