c5af56dd94ba80d26dd84defd4eca9beca22b1fe
Matheus Mello <mathgfmello@gmail.com>
2025-03-02 17:14:01 -0300

    feat: Adicionado ternarios

---
+++ src/main/java/Condicoes/Ternarios.java
@@ -0,0 +1,19 @@
+package Condicoes;
+
+public class Ternarios {
+    public static void main(String[] args) {
+        /*
+        * Ternarios: São maneiras de reduzir o codigo
+        * variavel = (condicao) ? valorSeVerdadeiro : valorSeFalso;
+        * */
+
+        short numeroDeMissoes = 18;
+        String nivelDoNinja = (numeroDeMissoes >= 10) ? "Esse ninja esta com mais de 10 missoes" :
+                "Esse ninja esta com menos de 10 missoes";
+
+        System.out.println(nivelDoNinja);
+
+
+    }
+
+}
