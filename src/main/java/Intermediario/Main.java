4860993e081b2a01768ae9d5b5e7bdff2fb7d85d
Matheus Mello <mathgfmello@gmail.com>
2025-03-07 20:13:05 -0300

    feat: Adicionado Métodos ao objeto

--- src/main/java/Intermediario/Main.java
+++ src/main/java/Intermediario/Main.java
@@ -8,6 +8,14 @@ public class Main {
         Sasuke.idade = 16;
         Sasuke.aldeia = "Konoha";
 
+        // Aplicando um método ao o objeto
+        Sasuke.ShariganAtivado();
+        String chamandoMetodo = Sasuke.EuSouUmninja();
+        System.out.println(chamandoMetodo);
+
+        int quatoTempoFalta = Sasuke.anosParaSeTornarHokage(25);
+        System.out.println("Você tem: " + Sasuke.idade + " Faltam no mínimo: " + quatoTempoFalta + " anos para se tornar Hokage");
+
         System.out.println("Nome: " + Sasuke.nome);
         System.out.println("Idade: " + Sasuke.idade);
         System.out.println("Aldeia: " + Sasuke.aldeia);
--- src/main/java/Intermediario/Ninja.java
+++ src/main/java/Intermediario/Ninja.java
@@ -6,4 +6,32 @@ public class Ninja {
      String aldeia;
      int idade;
 
+
+
+     // Criar um método publico personalizado
+
+     /*
+     * O metodo VOID não retorna valor nenhum
+     * */
+
+     public void ShariganAtivado(){
+            System.out.println("O Sharigan foi ativado");
+     }
+
+     /*
+     * O método String obrigatorimente deve retornar um valor do tipo String
+     * */
+
+     public String EuSouUmninja(){
+            return "Eu sou um ninja";
+     }
+
+     /*
+     * Metodo INT vai ter que retornar um valor do tipo inteiro
+     * */
+
+     public int anosParaSeTornarHokage(int IdadeMinimaParaSerHokage){
+            return IdadeMinimaParaSerHokage - idade;
+     }
+
 }
